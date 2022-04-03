1e940b41d4fa993a4370704034db7cbd180c2336
Sandeepbh07 <cheetrirocker07@gmail.com>
2022-04-03 11:03:26 +0545

    Boilerplate made

---
+++ controllers/auth.js
@@ -0,0 +1,1 @@
+exports.createOrUpdateUser = (req, res) => {};
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { readdirSync } = require("fs");
+require("dotenv").config();
+
+//app
+const app = express();
+
+//db
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB CONNECTED"))
+  .catch((err) => console.log(`DB connection error-${error}`));
+
+//middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "2mb" }));
+app.use(cors());
+
+//routes middleware(autoloading)
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
+
+//PORT
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server is running on ${port}`));
